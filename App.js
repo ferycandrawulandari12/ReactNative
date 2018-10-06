995b30eb7635c30c6860beda6accc981161c1d1f
fery candra wulandari <Ferycandra456@gmail.com>
2018-10-06 09:46:29 +0700

    bio+image

---
+++ App.js
@@ -0,0 +1,58 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          Nama : Fery Candra Wulandari
+        </Text>
+        <Text style={styles.instructions}>
+          Kelas : XI RPL 2
+        </Text>
+	     <Text style={styles.instructions}>
+          No.Absen : 18
+        </Text>
+        <Image style={{width:300, height:450}}
+        source={require('./aku.jpeg')}/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+5
